4302df255f9b06615334450ad4f3e5d81143716a
Fernando Ferreira de Lima Filho <fferreiralf@ufrn.edu.br>
2022-09-21 17:56:22 -0300

    first commit

---
+++ src/main/java/com/example/serversecurity/ServerSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.serversecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSecurityApplication.class, args);
+	}
+
+}
